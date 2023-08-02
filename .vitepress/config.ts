efbea6eab9cc0e74b79e226203b28bc787fe8805
Alissa Tung <alissa-tung@outlook.com>
2023-08-02 11:38:41 +0800

    *: init

---
+++ .vitepress/config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title:
+    'BiCMR "AI for Mathematics: Formalization and Theorem Proving" Seminar',
+  description:
+    'BiCMR "AI for Mathematics: Formalization and Theorem Proving" Seminar Homepage',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Examples", link: "/markdown-examples" },
+    ],
+
+    sidebar: [
+      {
+        text: "Examples",
+        items: [
+          { text: "Markdown Examples", link: "/markdown-examples" },
+          { text: "Runtime API Examples", link: "/api-examples" },
+        ],
+      },
+    ],
+
+    socialLinks: [
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+  },
+});
